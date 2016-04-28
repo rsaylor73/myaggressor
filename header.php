5bb3037133b72dc0942d56a45cc92afe3bbd0b36
Robert Saylor <robert@customphpdesign.com>
2016-04-27 22:39:28 -0400

    development

--- header.php
+++ header.php
@@ -27,15 +27,12 @@ if (screenwidth > 1280) {
 </script>
 
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
 
 
   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-  <script src="js/bootstrap.js"></script>
 
 
 
