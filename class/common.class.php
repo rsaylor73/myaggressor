b3ec86c61923813163c1dfe5f61304e5e250aee8
Robert Saylor <robert@customphpdesign.com>
2016-05-07 14:07:50 -0400

    development

--- class/common.class.php
+++ class/common.class.php
@@ -134,7 +134,7 @@ class Common {
      <td valign="top"><table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="217">
        <tr>
          <td><br>
-         <input type="button" style="width:200px;" value="Update Profile" class="btn btn-primary" onclick="document.location.href='profile.php';"><br>
+         <input type="button" style="width:200px;" value="Update Profile" class="btn btn-primary" onclick="document.location.href='profile.php';"><br><br>
          <input type="button" style="width:200px;" value="My Reservations" class="btn btn-primary" onclick="document.location.href='myreservations.php';"><br>
 
 
