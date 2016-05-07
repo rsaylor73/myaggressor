6b1f2b5b1db608429850f515cac602e1f15611ca
Robert Saylor <robert@customphpdesign.com>
2016-05-07 15:43:04 -0400

    development

--- class/common.class.php
+++ class/common.class.php
@@ -186,7 +186,7 @@ class Common {
           
           <span id="myaggressor">7 Seas Progress Bar</span><br>
           <div class="progress">
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
               <span class="sr-only">30% Complete</span>
             </div>
           </div>        
