1b918dc750469820626c597dacc015055f9588ec
Robert Saylor <robert@customphpdesign.com>
2016-05-26 22:10:10 -0400

    development

---
+++ creature.php
@@ -0,0 +1,19 @@
+<?php
+if($_SERVER["HTTPS"] != "on")
+{
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+}
+
+
+include "settings.php";
+include "header.php";
+   $bg = "1";
+   include "search.php";
+	if (($_GET['section'] == "") && ($_POST['section'] == "")) {
+	   $common->save_creature();
+	}
+
+
+
+?>
