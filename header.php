95d76f3bc43fd2a12ffc3db9eb4d55ce1a281192
Robert Saylor <robert@customphpdesign.com>
2016-05-28 15:57:43 -0400

    development

--- header.php
+++ header.php
@@ -22,7 +22,7 @@ session_start();
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <!--<script src="https://code.jquery.com/jquery.js"></script>-->
   <script src="js/bootstrap.min.js"></script>
-  <!--<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>-->
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
 
 
