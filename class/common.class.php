f7c6cca7b553cb74cea4e76a821d75909d5c2272
Robert Saylor <robert@customphpdesign.com>
2016-05-28 16:07:47 -0400

    development

--- class/common.class.php
+++ class/common.class.php
@@ -655,44 +655,33 @@ class Common {
             $checked = "checked";
           }
           print "<td><input type=\"checkbox\" name=\"id$row[id]\" value=\"checked\" $checked></td><td>
-            <form name=\"myform$row[id]\" style=\"display:inline\">
             <input type=\"hidden\" name=\"id\" value=\"$row[id]\">
-            <a href=\"#\" data-toggle=\"modal\" data-target=\"#basicModal\">$row[title]</a>
-            </form>
-            </td></tr>";
-        }
-
-        print "</table><input type=\"submit\" value=\"Save\" class=\"btn btn-success\"></form>";
-
-        ?>
-        <script>
-        function show_creature() {
-
-        }
-        </script>
-
-        <?php
+            <a href=\"#\" data-toggle=\"modal\" data-target=\"#basicModal$row[id]\">$row[title]</a>
+            ";
 
         print '
-    <div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+    <div class="modal fade" id="basicModal'.$row['id'].'" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title" id="myModalLabel">Basic Modal</h4>
+            <h4 class="modal-title" id="myModalLabel">'.$row['title'].'</h4>
           </div>
           <div class="modal-body">
             <h3>Modal Body</h3>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
           </div>
         </div>
       </div>
     </div>
         ';
 
+            print "</td></tr>";
+        }
+
+        print "</table><input type=\"submit\" value=\"Save\" class=\"btn btn-success\"></form>";
 
 
       }
