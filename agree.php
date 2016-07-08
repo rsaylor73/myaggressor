73e72e1a494b8decaa22920bb85507573c1ccb58
root <root@linux2.aggressor.com>
2016-07-08 14:55:14 -0400

    TOS

---
+++ agree.php
@@ -0,0 +1,15 @@
+<?php
+if($_SERVER["HTTPS"] != "on")
+{
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+}
+
+include "settings.php";
+include "header.php";
+   $bg = "1";
+   include "search.php";
+
+   $common->agree();
+?>
+
